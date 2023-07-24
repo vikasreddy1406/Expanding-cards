45296adc905f17ccc6053b0dfa8ffe7f3905a7b3
vikasreddy1406 <vikassai345@gmail.com>
2023-07-24 16:25:52 +0530

    expanding-cards

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActive();
+        panel.classList.add("active");
+    });
+});
+
+
+const removeActive = () => {
+    panels.forEach(panel => {
+        panel.classList.remove("active");
+    });
+};
+
+
